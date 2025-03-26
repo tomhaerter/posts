a543e869a488ae138ce2f0e3b6129c7fd32cab79
Tom Härter <tomuhaerter@gmail.com>
2025-03-26 10:17:57 +0100

    tailwind css

---
+++ app/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="text-red-500">hello world</div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-03-26",
+  devtools: { enabled: true },
+  css: ["@/assets/main.css"],
+  future: {
+    compatibilityVersion: 4,
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
