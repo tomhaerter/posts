241beaa4e54e6ec5aa77083df06d16da78737efe
Tom Härter <tomuhaerter@gmail.com>
2025-03-26 10:35:59 +0100

    login

---
+++ app/pages/login.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const email = ref("");
+const password = ref("");
+
+async function login() {
+  console.log("login", email.value, password.value);
+
+  await $fetch("/api/login", {
+    method: "POST",
+    body: {
+      email: email.value,
+      password: password.value,
+    },
+  });
+}
+</script>
+
+<template>
+  <form @submit.prevent="login" class="flex flex-col">
+    <div>{{ email }}</div>
+    <input
+      v-model="email"
+      type="email"
+      name="email"
+      placeholder="Email"
+      class="border"
+    />
+    <input
+      v-model="password"
+      type="password"
+      name="password"
+      placeholder="Password"
+      class="border"
+    />
+    <button type="submit" class="bg-black text-white">Login</button>
+  </form>
+</template>
